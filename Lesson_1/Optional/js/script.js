33bcb011941dc157d5ad22342bc635cd999e765d
Dmitry Zharikov <xenoffex@gmail.com>
2019-06-06 18:12:39 +0300

    lesson one opt task

--- Lesson_1/Optional/js/script.js
+++ Lesson_1/Optional/js/script.js
@@ -1,14 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    let imgs = document.querySelectorAll('IMG');
+    let imgs = document.querySelector('.img');
     let images = ['img/img_1.jpg', 'img/img_2.jpg', 'img/img_3.jpg'];
 
-    let firstPormise = new Promise((resolve, reject) => {
-        
+    let promises = [];
+
+    images.map(elem => {
+        promises.push(new Promise( (resolve, reject) => {
+            let someImage = new Image();
+
+            someImage.addEventListener('load', () => {
+                resolve();
+            });
+
+            someImage.addEventListener('error', () => {
+                reject(console.error(`the path to unloaded image is ${elem}`));
+            });
+
+            someImage.src = elem;
+            imgs.appendChild(someImage);
+        }));
     });
 
-    firstPormise
-        .then(console.log('yeah'))
-        .catch(console.log('F'));
+    Promise.all(promises).then(e => console.log("All pics loaded")).catch(e => console.error('An error has occurred'));
 });
