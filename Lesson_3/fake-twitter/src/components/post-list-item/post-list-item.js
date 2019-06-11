409470ec6a3bb4965a06fecbada38f18246e2afc
Dmitry Zharikov <xenoffex@gmail.com>
2019-06-11 18:52:00 +0300

    lesson 4 with Optional task

--- Lesson_3/fake-twitter/src/components/post-list-item/post-list-item.js
+++ Lesson_3/fake-twitter/src/components/post-list-item/post-list-item.js
@@ -1,32 +1,106 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './post-list-item.css';
 
-const PostListItem = () => {
-    const postTime = new Date().toLocaleString();
-    return (
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Hello World!
+export default class PostListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,
+            like: false,
+            edit: false,
+            edited: true
+        }
+        this.editedText = this.props.label;
+        this.postTime = new Date().toLocaleString();
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
+        this.onEdit = this.onEdit.bind(this);
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }))
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+    
+    onEdit() {
+        this.setState(({edit, edited}) => ({
+            edit: !edit,
+            edited: !edited
+        }))
+        if (this.state.edited === false) {
+            this.applyChanges();
+        }
+    }
+
+    applyChanges() {
+        return this.editedText = document.querySelector('.edited-item-value').value; 
+    }
+
+    showText = () => {
+        return (
+            <span className="app-list-item-label"
+            onClick={this.onLike}>
+                {this.editedText}
             </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button 
-                    type="button" 
-                    className="btn-star btn-sm">
-                    <i className="fa fa-star"></i>
-                </button>
-                <button 
-                    type="button" 
-                    className="btn-trash btn-sm">
-                    <i className="fa fa-trash-o"></i>
-                </button>
-                <i className="fa fa-heart"></i>
-                <div>
-                    <i className="fa fa-clock-o"></i>
-                    {postTime}
+        )
+    }
+
+    editText = () => {
+        return (
+            <input 
+            type="text"
+            placeholder={this.editedText}
+            className="edited-item-value app-list-item-label"/>
+        )
+    }
+
+    render () {
+        const {important, like, edit} = this.state;
+        let classNames = 'app-list-item d-flex justify-content-between';
+
+        if (important) {
+            classNames += ' important';
+        }
+
+        if (like) {
+            classNames += ' like';
+        }
+
+        return (
+            <div className={classNames}>
+                {(edit) ? this.editText() : this.showText()}
+                <div className="d-flex justify-content-center align-items-center">
+                    <button
+                        type="button"
+                        className="btn-pencil btn-sm"
+                        onClick={this.onEdit}>
+                        <i className="fa fa-pencil"/>
+                    </button>
+                    <button 
+                        type="button" 
+                        className="btn-star btn-sm"
+                        onClick={this.onImportant}>
+                        <i className="fa fa-star"></i>
+                    </button>
+                    <button 
+                        type="button" 
+                        className="btn-trash btn-sm">
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                    <i className="fa fa-heart"></i>
+                    <div>
+                        <i className="fa fa-clock-o"></i>
+                        {this.postTime}
+                    </div>
                 </div>
             </div>
-        </li>
-    )
+        )
+    }
 }
-
-export default PostListItem;
--- Lesson_3/fake-twitter/src/components/post-list/post-list.js
+++ Lesson_3/fake-twitter/src/components/post-list/post-list.js
@@ -3,12 +3,24 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+    const elements = posts.map((item) => {
+        if (typeof(item) == 'object') {
+            const {id, ...itemProps} = item;
+            return (
+                <li key={id} className='list-group-item'>
+                    <PostListItem {...itemProps}/>
+                </li>
+            )
+        } else {
+            return null
+        }
+    });
+
     return (
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 }
